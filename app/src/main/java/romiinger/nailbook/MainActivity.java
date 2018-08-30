9e9dc3e55ab9070ba1208cf9b477070024a9c1ba
ROMINA INGER <romiinger@gmail.com>
2018-08-30 13:06:25 +0300

    Initial commit

---
+++ app/src/main/java/romiinger/nailbook/MainActivity.java
@@ -0,0 +1,13 @@
+package romiinger.nailbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
