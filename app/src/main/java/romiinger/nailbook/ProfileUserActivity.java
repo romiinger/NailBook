5aa545556bef9b707c2b85422eeb5c357c0dc2fe
RapidEU <shlomib1991@gmail.com>
2018-09-21 18:56:28 +0300

    update layout

--- app/src/main/java/romiinger/nailbook/ProfileUserActivity.java
+++ app/src/main/java/romiinger/nailbook/ProfileUserActivity.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 import android.content.Intent;
 public class ProfileUserActivity extends  AppCompatActivity {
 
-    private static MyUser user;
+    //private static MyUser user;
     private static final String TAG = "ProfileUserActivity";
 
     @Override
@@ -18,17 +18,15 @@ public class ProfileUserActivity extends  AppCompatActivity {
         setContentView(R.layout.profile_activity);
         Log.d(TAG,"profile activity start");
         UserAdapter userAdapter = new UserAdapter();
-        user = userAdapter.getmUser();
-        Log.d(TAG,"user.getName()=" + user);
+
+        MyUser user = userAdapter.getmUser();
+        Log.d(TAG,"user=" + user);
         Log.d(TAG,"user.getName()=" + user.getName() );
 
-       // Intent Extra = getIntent();
-       // String textView = Extra.getStringExtra("UserInput");
 
-       // TextView UserInput = (TextView) findViewById(R.id.UserOutput);
-        //UserInput.setText(textView);
 
-        EditText editText = (EditText)findViewById(R.id.text_input_layout_fullName);
-        editText.setText(user.getName(), TextView.BufferType.EDITABLE);
+        TextView nameLayout = (TextView)findViewById(R.id.fullNameLayout);
+        String name = user.getName();
+        nameLayout.setText(name);
     }
 }
